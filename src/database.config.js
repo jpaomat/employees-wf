7d423eab7e6ffd737c85f29588db7d1befede04d
Jenny Matamoros <jpaomat@gmail.com>
2021-04-04 19:32:03 -0500

    Implementing docker-compose

--- src/database.config.js
+++ src/database.config.js
@@ -4,6 +4,16 @@ module.exports = {
         host: 'localhost',
         user: 'root',
         password: '',
-        database : 'apiemployees'
+        database : 'apiemployees',
+        port : '3306'
     }
+
+    // database: {
+    //     connectionLimit: 10,
+    //     host: process.env.MYSQL_HOST || 'localhost',
+    //     user: process.env.MYSQL_USER || 'root',
+    //     password: process.env.MYSQL_PASS || '',
+    //     database : process.env.MYSQL_DB ||'apiemployees',
+    //     port : process.env.MYSQL_PORT ||'3306'
+    // }
 }
