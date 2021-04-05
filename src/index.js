b705b57bf93d50cd8e111b03d751994f54821fea
Jenny Matamoros <jpaomat@gmail.com>
2021-04-05 12:06:48 -0500

    Fixing cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+
 
 const app = express();
 
+app.use(cors());
 app.set('port', process.env.PORT || 4000);
 
 //Middlesware
