5b625bfcdacafdfae86966840b41e2e5c0cde503
Jenny Matamoros <jpaomat@gmail.com>
2021-04-04 20:11:53 -0500

    Fixing configuration docker-compose-yml file

--- src/database.config.js
+++ src/database.config.js
@@ -1,19 +1,10 @@
 module.exports = {
     database: {
         connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database : 'apiemployees',
-        port : '3306'
+        host: process.env.MYSQL_HOST || 'localhost',
+        user: process.env.MYSQL_USER || 'root',
+        password: process.env.MYSQL_PASS || '',
+        database : process.env.MYSQL_DB ||'apiemployees',
+        port : process.env.MYSQL_PORT ||'3306'
     }
-
-    // database: {
-    //     connectionLimit: 10,
-    //     host: process.env.MYSQL_HOST || 'localhost',
-    //     user: process.env.MYSQL_USER || 'root',
-    //     password: process.env.MYSQL_PASS || '',
-    //     database : process.env.MYSQL_DB ||'apiemployees',
-    //     port : process.env.MYSQL_PORT ||'3306'
-    // }
 }
